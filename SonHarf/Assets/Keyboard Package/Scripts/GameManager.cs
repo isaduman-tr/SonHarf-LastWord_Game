3f54c281d2e456db47eb5f87f1cadde91869031a
isa duman <114006114+isaduman-tr@users.noreply.github.com>
2025-01-02 18:21:26 +0300

    sonhal
    sonhal

--- SonHarf/Assets/Keyboard Package/Scripts/GameManager.cs
+++ SonHarf/Assets/Keyboard Package/Scripts/GameManager.cs
@@ -1,35 +1,65 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
-    [SerializeField] TextMeshProUGUI textBox;
-    [SerializeField] TextMeshProUGUI printBox;
+    [SerializeField] TextMeshProUGUI inputTextBox;
+    [SerializeField] TextMeshProUGUI outputTextBox;
+    [SerializeField] TextMeshProUGUI newWordTextBox; // Yeni kelimeyi yazdýrmak için
+
+    private Dictionary<char, List<string>> wordDictionary;
 
     private void Start()
     {
         Instance = this;
-        printBox.text = "";
-        textBox.text = "";
+        outputTextBox.text = "";
+        inputTextBox.text = "";
+        newWordTextBox.text = "";
+
+        // Kelime listesini oluþturun
+        wordDictionary = new Dictionary<char, List<string>>()
+        {
+            { 'a', new List<string> { "araba", "ayva", "armut" } },
+            { 'b', new List<string> { "balýk", "biber", "bardak" } },
+            { 'k', new List<string> { "kedi", "kaplumbaða", "kale" } },
+            // Diðer harfler için benzer listeler ekleyin
+        };
     }
 
     public void DeleteLetter()
     {
-        if(textBox.text.Length != 0) {
-            textBox.text = textBox.text.Remove(textBox.text.Length - 1, 1);
+        if (inputTextBox.text.Length != 0)
+        {
+            inputTextBox.text = inputTextBox.text.Remove(inputTextBox.text.Length - 1, 1);
         }
     }
 
     public void AddLetter(string letter)
     {
-        textBox.text = textBox.text + letter;
+        inputTextBox.text = inputTextBox.text + letter;
     }
 
     public void SubmitWord()
     {
-        printBox.text = textBox.text;
-        textBox.text = "";
-        // Debug.Log("Text submitted successfully!");
+        string inputWord = inputTextBox.text.ToLower();
+        inputTextBox.text = "";
+
+        if (inputWord.Length > 0)
+        {
+            char lastChar = inputWord[inputWord.Length - 1];
+
+            if (wordDictionary.ContainsKey(lastChar))
+            {
+                List<string> possibleWords = wordDictionary[lastChar];
+                string newWord = possibleWords[Random.Range(0, possibleWords.Count)];
+                newWordTextBox.text = newWord; // Yeni kelimeyi bu bileþene yazdýr
+            }
+            else
+            {
+                newWordTextBox.text = "Bu harfle baþlayan kelime bulunamadý.";
+            }
+        }
     }
 }
