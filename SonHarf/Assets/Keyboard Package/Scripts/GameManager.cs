4b85fe0d0795727818247d690b4c771e077a212a
isa duman <114006114+isaduman-tr@users.noreply.github.com>
2025-01-02 21:05:26 +0300

    Last
    Kütüphane .list dosyaları ile eklendi ve submitword kısmında düzenlemeler yapıldı şuan çalışıyor

--- SonHarf/Assets/Keyboard Package/Scripts/GameManager.cs
+++ SonHarf/Assets/Keyboard Package/Scripts/GameManager.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using TMPro;
 using System.Collections.Generic;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     [SerializeField] TextMeshProUGUI inputTextBox;
     [SerializeField] TextMeshProUGUI outputTextBox;
-    [SerializeField] TextMeshProUGUI newWordTextBox; // Yeni kelimeyi yazdýrmak için
+    [SerializeField] TextMeshProUGUI newWordTextBox;
 
     private Dictionary<char, List<string>> wordDictionary;
 
@@ -18,14 +19,25 @@ public class GameManager : MonoBehaviour
         inputTextBox.text = "";
         newWordTextBox.text = "";
 
-        // Kelime listesini oluþturun
-        wordDictionary = new Dictionary<char, List<string>>()
+        wordDictionary = new Dictionary<char, List<string>>();
+
+        // Dosyalardan kelimeleri yükle
+        LoadWordsFromFiles();
+    }
+
+    private void LoadWordsFromFiles()
+    {
+        string path = Path.Combine(Application.streamingAssetsPath, "{0}.list");
+
+        for (char c = 'a'; c <= 'z'; c++)
         {
-            { 'a', new List<string> { "araba", "ayva", "armut" } },
-            { 'b', new List<string> { "balýk", "biber", "bardak" } },
-            { 'k', new List<string> { "kedi", "kaplumbaða", "kale" } },
-            // Diðer harfler için benzer listeler ekleyin
-        };
+            string fileName = string.Format(path, c);
+            if (File.Exists(fileName))
+            {
+                string[] words = File.ReadAllLines(fileName);
+                wordDictionary[c] = new List<string>(words);
+            }
+        }
     }
 
     public void DeleteLetter()
@@ -50,15 +62,41 @@ public class GameManager : MonoBehaviour
         {
             char lastChar = inputWord[inputWord.Length - 1];
 
+            // Ýlk olarak, kullanýcýnýn girdiði kelimenin mevcut olup olmadýðýný kontrol edelim.
+            bool isWordInList = false;
+            foreach (var list in wordDictionary.Values)
+            {
+                if (list.Contains(inputWord))
+                {
+                    isWordInList = true;
+                    break;
+                }
+            }
+
+            if (!isWordInList)
+            {
+                newWordTextBox.text = "Bu kelime listede yok.";
+                return;
+            }
+
+            // Eðer kelime geçerli ise devam et
             if (wordDictionary.ContainsKey(lastChar))
             {
                 List<string> possibleWords = wordDictionary[lastChar];
-                string newWord = possibleWords[Random.Range(0, possibleWords.Count)];
-                newWordTextBox.text = newWord; // Yeni kelimeyi bu bileþene yazdýr
+                if (possibleWords.Count > 0)
+                {
+                    // Yeni kelimeyi seç ve göster
+                    string newWord = possibleWords[Random.Range(0, possibleWords.Count)];
+                    newWordTextBox.text = newWord;
+                }
+                else
+                {
+                    newWordTextBox.text = "Listede yok, tekrar deneyiniz.";
+                }
             }
             else
             {
-                newWordTextBox.text = "Bu harfle baþlayan kelime bulunamadý.";
+                newWordTextBox.text = "Listede yok, tekrar deneyiniz.";
             }
         }
     }
